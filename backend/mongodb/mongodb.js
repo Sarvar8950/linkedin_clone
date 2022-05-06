a00e89a1b10f6a5667acbeab663a5f41be567b0a
shivlingbhange2510 <shivlingbhange2510@gmail.com>
2022-05-06 10:32:36 -0700

    'code-added-for-post'

--- backend/mongodb/mongodb.js
+++ backend/mongodb/mongodb.js
@@ -10,12 +10,10 @@ class Mongo {
         // db.createUser({user:'shiv', pwd:'shiv@123',roles:['dbOwner']})
 
         mongoose.connect(
-            `mongodb://127.0.0.1:27017/test`
+      `mongodb+srv://shivlingbhange2510:Shivling@123cluster0.is5o6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
         //    `mongodb+srv://shivlingbhange2510:
         //    Shivling@123@first-app-cluster.cfvzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
             // `mongodb://127.0.0.1:27017/test`,
-            // mongodb://shiv:shiv@123localhost:27017
-            // `mongodb://shiv:shiv@123@localhost:27017/movies`,
             //  {useNewUrlParser: true}
              )
         .then(()=>{
